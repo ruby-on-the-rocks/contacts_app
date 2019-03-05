b33b6bdb8085c97f3230e63ea0a057117d702c38
Peter Jang <peter.x.jang@gmail.com>
2019-03-04 18:46:18 -0600

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Contact.create(first_name: "Peter", last_name: "Jang", email: "peter@email.com", phone_number: "3535435435")
+Contact.create(first_name: "Jay", last_name: "Wengrow", email: "jay@email.com", phone_number: "4535435435")
+Contact.create(first_name: "Dani", last_name: "Zaghian", email: "dani@email.com", phone_number: "5535435435")
