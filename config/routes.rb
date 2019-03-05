6a9eade677ddec8b9c5bb926a4457fc416dce45e
Peter Jang <peter.x.jang@gmail.com>
2019-03-04 19:08:24 -0600

    Add RESTful contacts actions

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,11 @@ Rails.application.routes.draw do
   # namespace :api do
   #   get "/photos" => "photos#index"
   # end
+  namespace :api do
+    get "/contacts" => "contacts#index"
+    post "/contacts" => "contacts#create"
+    get "/contacts/:id" => "contacts#show"
+    patch "/contacts/:id" => "contacts#update"
+    delete "/contacts/:id" => "contacts#destroy"
+  end
 end
