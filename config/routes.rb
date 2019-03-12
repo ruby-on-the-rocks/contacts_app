975306e285212b66572708e0106f58964f599e47
Peter Jang <peter.x.jang@gmail.com>
2019-03-11 21:12:23 -0500

    Add sessions create action (login)

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   # end
   namespace :api do
     post "/users" => "users#create"
+
+    post "/sessions" => "sessions#create"
     
     get "/contacts" => "contacts#index"
     post "/contacts" => "contacts#create"
